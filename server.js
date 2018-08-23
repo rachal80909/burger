4234720d45c5caac12860e5506133a183ce89da1
Rachal Metz <[rachal80909@yahoo.com]>
2018-08-22 18:11:49 -0600

    adding dependencies

--- server.js
+++ server.js
@@ -2,3 +2,30 @@
     *
     express *
     body - parser
+npm
+
+// Include the geocoder and inquirer NPM packages
+var express = require("express");
+var bodyparser = require("bodyparser");
+
+// Prompt the user to provide location information.
+inquirer.prompt([
+
+    {
+        type: "input",
+        name: "userInput",
+        message: "Which location or landmark would you like to geocode?"
+    }
+
+    // After the prompt, store the user's response in a variable called location.
+]).then(function(location) {
+
+    // console.log(location.userInput);
+
+    // Then use the Google Geocoder to Geocode the address
+    geocoder.geocode(location.userInput, function(err, data) {
+
+        console.log(JSON.stringify(data, null, 2));
+    });
+
+});
