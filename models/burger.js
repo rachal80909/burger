8f1f4cd124e7e67c637b9cbd971f8a59bd2f9167
Rachal Metz <[rachal80909@yahoo.com]>
2018-08-26 14:16:14 -0600

    updating file structure

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,13 @@
 var orm = require("../config/orm");
+
+*
+Inside `burger.js`,
+    import `orm.js`
+into `burger.js`
+
+*
+Also inside `burger.js`, create the code that will call the ORM functions using burger specific input
+for the ORM.
+
+*Export at the end of the `burger.js`
+file.
